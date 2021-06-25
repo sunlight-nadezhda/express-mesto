b27f12eeaadc6b2406b9833bef3b4655a4cd9994
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-06-25 22:37:50 +0300

    feat: создать токен и отправить его через тело ответа

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -129,5 +130,38 @@ module.exports.updateAvatar = (req, res) => {
         return res.status(ERROR_CODE).send({ message: 'Проверьте введенные данные' });
       }
       return res.status(500).send({ message: err.message });
+    });
+};
+
+// Аутентификация пользователя
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            // хеши не совпали — отклоняем промис
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+
+          // аутентификация успешна
+          const token = jwt.sign(
+            { _id: user._id },
+            'some-secret-key',
+            { expiresIn: '7d' },
+          );
+          return res.send({ token });
+        });
+    })
+    .catch((err) => {
+      res
+        .status(401)
+        .send({ message: err.message });
     });
 };
