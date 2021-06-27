15759154a0d558a928d398db64327f7d0de0a42e
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-06-27 23:00:02 +0300

    feat: добавить select

--- models/user.js
+++ models/user.js
@@ -34,6 +34,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 8,
+    select: false,
   },
 },
 {
@@ -42,6 +43,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.statics.findUserByCredentials = function f(email, password) {
   return this.findOne({ email })
+    .select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
