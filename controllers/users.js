0aff62def93fb7c07d4c121b595a9efb7492b2e0
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-06-29 00:11:13 +0300

    feat: убрать хеш пароля из тела ответа при создании нового пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -54,7 +54,15 @@ module.exports.createUser = (req, res, next) => {
       email,
       password: hash,
     }))
-    .then((user) => res.send(user))
+    .then((user) => {
+      res.send({
+        _id: user._id,
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+        email: user.email,
+      });
+    })
     .catch((err) => {
       if (err.name === 'ValidationError') {
         throw new NoValidateError('Проверьте введенные данные');
